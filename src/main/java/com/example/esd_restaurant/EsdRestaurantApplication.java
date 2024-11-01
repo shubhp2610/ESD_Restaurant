addc64e47a7d27524cf18a4bd5364ab6643114fb
shubh patel <shubhpatel2610@outlook.com>
2024-11-01 23:29:18 +0530

    first commit

---
+++ src/main/java/com/example/esd_restaurant/EsdRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.esd_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/esd_restaurant/EsdRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esd_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
