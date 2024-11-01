71e82ff677527ef324d7f9d2884abdb43e69a93d
shubh patel <shubhpatel2610@outlook.com>
2024-11-01 23:52:55 +0530

    Added SQL config + defined customer entity

---
+++ src/main/java/com/example/esd_restaurant/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.example.esd_restaurant.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "pincode")
+    private Long pincode;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+}
