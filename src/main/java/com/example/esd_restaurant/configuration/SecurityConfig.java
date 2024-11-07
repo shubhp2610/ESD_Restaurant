076e8af5942977a02c41734ce11953c2c4e8844e
shubh patel <shubhpatel2610@outlook.com>
2024-11-08 00:57:41 +0530

    Added password encryption logic to create and login endpoint

---
+++ src/main/java/com/example/esd_restaurant/configuration/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.esd_restaurant.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig  implements WebMvcConfigurer {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/esd_restaurant/service/CustomerService.java
+++ src/main/java/com/example/esd_restaurant/service/CustomerService.java
@@ -2,8 +2,10 @@ package com.example.esd_restaurant.service;
 
 import com.example.esd_restaurant.dto.CustomerRequest;
 import com.example.esd_restaurant.entity.Customer;
+import com.example.esd_restaurant.helper.JWTHelper;
 import com.example.esd_restaurant.mapper.CustomerMapper;
 import com.example.esd_restaurant.repo.CustomerRepo;
+import com.example.esd_restaurant.helper.EncryptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
-
+    private final EncryptionService encryptionService;
+    private final JWTHelper jwHelper;
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
     public Customer createCustomer(CustomerRequest.CustomerCreateRequest request) {
         Customer customer = mapper.toEntity(request);
+        customer.setPassword(encryptionService.encode(customer.getPassword()));
         customer = repo.save(customer);
         return customer;
     }
@@ -26,8 +30,8 @@ public class CustomerService {
 
         if (optionalCustomer.isPresent()) {
             Customer customer = optionalCustomer.get();
-            if (customer.getPassword().equals(request.password())) {
-                return "User logged in successfully";
+            if (encryptionService.validates(request.password(), customer.getPassword())) {
+                return jwHelper.generateToken(customer.getEmail());
             } else {
                 return "Invalid password";
             }
