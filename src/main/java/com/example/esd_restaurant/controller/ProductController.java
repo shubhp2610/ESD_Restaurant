4a1a4b825a15d588c3f0cc016798d90337256e65
shubh patel <shubhpatel2610@outlook.com>
2024-11-18 23:58:00 +0530

    Implemented product CRUD operations: Get all, get single product, update product and delete product

--- src/main/java/com/example/esd_restaurant/controller/ProductController.java
+++ src/main/java/com/example/esd_restaurant/controller/ProductController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/product")
@@ -21,4 +23,35 @@ public class ProductController {
         return ResponseEntity.ok(productService.createProduct(productCreateRequest));
     }
 
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id , @RequestBody @Valid ProductRequest.ProductRequestBody productUpdateRequest) {
+        Product product = productService.updateProduct(id, productUpdateRequest);
+        if(product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        Boolean deleted = productService.deleteProduct(id);
+        if (!deleted) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok("Product Deleted Successfully!");
+    }
 }
--- src/main/java/com/example/esd_restaurant/service/ProductService.java
+++ src/main/java/com/example/esd_restaurant/service/ProductService.java
@@ -7,6 +7,9 @@ import com.example.esd_restaurant.repo.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -17,6 +20,33 @@ public class ProductService {
         product = repo.save(product);
         return product;
     }
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product updateProduct(Long id, ProductRequest.ProductRequestBody request) {
+        Product product = getProductById(id);
+        if(product != null) {
+            product.setName(request.name());
+            product.setPrice(request.price());
+            product.setUpdatedOn(LocalDateTime.now());
+            }
+        return product!=null?repo.save(product):null;
+    }
+
+    public boolean deleteProduct(Long id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
 
 }
 
